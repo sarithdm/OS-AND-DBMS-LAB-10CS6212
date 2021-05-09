469c584983ae5712ff67758e456d197380ec2d01
sajina567 <83805498+sajina567@users.noreply.github.com>
2021-05-09 20:23:36 +0530

    Add files via upload

---
+++ sajina/assignment1/program1.sh
@@ -0,0 +1,19 @@
+pwd
+mkdir adir
+pwd
+ls 
+cd adir
+pwd
+echo "first file created ">testfile.txt
+echo "verify that file exist"
+ls 
+echo "list the contents of testfile to screen"
+cat testfile.txt
+cp testfile.txt secondfile.txt
+echo "verify both the files are exists"
+ls 
+cat testfile.txt
+cat secondfile.txt
+
+~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                                                       ~                                                                                                                                        
+
