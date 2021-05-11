1d94463efe690ce8fc13ec8e8c19f5e7f90636ff
Haritha57 <harithamv57@gmail.com>
2021-05-11 16:35:15 +0530

    file copied

---
+++ Haritha/Assignment2/excercise2.sh
@@ -0,0 +1,11 @@
+rm ../Assignment1/testfile.txt
+cd ../Assignment1/
+ls -lrt
+clear
+mv secondfile.txt thefile.txt
+stat thefile.txt
+cp thefile.txt ../Assignment2
+rm ../Assignment1/thefile.txt
+ls -lrt
+cp ../Assignment2/thefile.txt .
+ls -lrt
