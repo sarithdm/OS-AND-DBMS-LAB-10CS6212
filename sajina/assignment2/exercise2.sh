543470067a0c87d963708ca7630ef8932acdfb97
sajina567 <sajinaradhakrishnan@gmail.com>
2021-05-11 17:49:41 +0530

    copy

--- sajina/assignment2/exercise2.sh
+++ sajina/assignment2/exercise2.sh
@@ -1,24 +1,24 @@
-echo "delete the testfile"
+
 rm ../assignment1/testfile.txt
-echo "verify the testfile is deleted"
+
 cd ../assignment1/
 ls 
-echo "clear the window"
+
 clear
-echo "Rename secondfile to thefile."
+
 mv secondfile.txt thefile.txt
-echo "Issue the command to find out how large thefile is. How big is it"
+
 stat  thefile.txt
-echo "Copy thefile to your home directory"
+
 cp  thefile.txt ../assignment2/
-echo "Remove thefile from the current directory."
+
 rm   ../assignment1/thefile.txt
-echo "verify that removed or not"
+
 ls 
-echo "Copy thefile from your home directory to the current directory."
+
 cp  ../assignment2/thefile.txt
-echo " Verify that the file has been copied from your home directory to the current directory."
-ls 	
+
+ls -lrt 	
 
 
 
