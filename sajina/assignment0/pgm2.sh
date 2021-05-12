6000d21ada2cde16c863cf2c09670925045a7838
sajina567 <sajinaradhakrishnan@gmail.com>
2021-05-12 09:15:17 +0530

    second program

---
+++ sajina/assignment0/pgm2.sh
@@ -0,0 +1,11 @@
+rm ../assignment1/testfile.txt
+cd ../assignment1/
+ls
+clear
+mv secondfile.txt thefile.txt
+stat thefile.txt
+cp thefile.txt ../assignment0
+rm ../assignment1/thefile.txt
+ls
+cp ../assignment0/thefile.txt .
+ls
