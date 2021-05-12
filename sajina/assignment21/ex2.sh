4be66d2bd9dd88a7279a3fa39bb449f32ca0d413
sajina567 <sajinaradhakrishnan@gmail.com>
2021-05-12 08:45:19 +0530

    the second ex

---
+++ sajina/assignment21/ex2.sh
@@ -0,0 +1,11 @@
+rm ../assignment1/testfile.txt
+cd ../assignment1/
+ls
+clear
+mv secondfile.txt thefile.txt
+stat thefile.txt
+cp thefile.txt ../assignment21
+rm ../assignment1/thefile.txt
+ls
+cp ../assignment21/thefile.txt .
+ls
