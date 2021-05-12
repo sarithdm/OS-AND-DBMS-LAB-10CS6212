cd3846840c41f3d4698b4a14599ca0ec3f300c47
sajina567 <sajinaradhakrishnan@gmail.com>
2021-05-12 17:08:33 +0530

    copy

---
+++ sajina/assi2/pgm2.sh
@@ -0,0 +1,11 @@
+rm ../assignment1/testfile.txt
+cd ../assignment1/
+ls
+clear
+mv secondfile.txt thefile.txt
+stat thefile.txt
+cp thefile.txt ../assi2
+rm ../assignment1/thefile.txt
+ls
+cp ../assi2/thefile.txt .
+ls
