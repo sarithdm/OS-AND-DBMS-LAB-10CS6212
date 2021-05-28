83a485b3b272b8e32d15c1f7a9c5d57aca3ca321
Haritha57 <harithamv57@gmail.com>
2021-05-28 14:37:00 +0530

    mywork

---
+++ Haritha/lab1/2/pgm4.sh
@@ -0,0 +1,12 @@
+#Shell script program chek file exist in a directory
+
+cd "$1"
+for i in "$1";
+do
+	if [ -a "$2" ];
+	then
+		echo "File '$2' exist in $1"
+	else
+		echo "File '$2' does not exist in $1"
+	fi
+done
