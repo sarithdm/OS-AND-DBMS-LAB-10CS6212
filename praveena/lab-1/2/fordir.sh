1a677afff24cae9c2e4c71292c81c546d532330c
praveena <praveenabnair28@gmail.com>
2021-05-28 10:18:07 +0530

    for.sh

---
+++ praveena/lab-1/2/fordir.sh
@@ -0,0 +1,16 @@
+#shell script program to count number of files in a directory
+#!/bin/bash
+cd "$1"
+dir=0
+file=0
+for myvar in *;
+do
+	if [ -d "$myvar" ];
+	then
+		dir=$((dir+1))
+	else
+		file=$((file+1))
+	fi
+done
+echo "Directories $dir"
+echo "Files $file"
