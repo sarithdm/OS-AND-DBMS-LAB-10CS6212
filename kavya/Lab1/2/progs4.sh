cdc840236c1902ccd0e81efa785c7784051ac9d8
kavya <kavya.murali2013@gmail.com>
2021-05-30 09:37:01 +0530

    programsin2

---
+++ kavya/Lab1/2/progs4.sh
@@ -0,0 +1,11 @@
+#shell script program to check a file exist in a directory
+cd "$1"
+for c in "$1";
+do 
+	if [ -a "$2" ];
+	then
+		echo "File '$2' exist in $1"
+	else
+		echo "File '$2' does not exist in $1"
+	fi
+done
