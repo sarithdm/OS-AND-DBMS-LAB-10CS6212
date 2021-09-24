e0a0ec1b150b588a93b1e97485d618bdfe13bd6b
AbhiramPanicker <83579053+AbhiramPanicker@users.noreply.github.com>
2021-09-24 11:11:32 +0530

    Delete practicetest directory

--- practicetest/1.countlines.sh
+++
@@ -1,8 +0,0 @@
-# Write a shell script to count the number of lines in each file
-
-
-find . -type f -name  "*" | while read FILE; do
-	count=$(wc -l < "$FILE")
-	echo "$FILE contains $count lines"
-done
-
--- practicetest/2.renametxtfiles.sh
+++
@@ -1,7 +0,0 @@
-#Write a shell script to rename all files in the directory with .txt extension to .dat 
-
-
-for f in *.txt;
-do
-mv -- "$f" "${f%.txt}.dat"
-done
